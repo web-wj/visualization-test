f98c326ac67685ac52f53aad1730c32f833ca0b0
Alex Wang <451959831@qq.com>
2024-05-22 22:48:02 +0800

    project: init

---
+++ visualization-test/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <!-- 左侧 -->
+    <div style=""><h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1></div>
+    <!-- 中间 -->
+    <div></div>
+    <!-- 右侧 -->
+    <div></div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
